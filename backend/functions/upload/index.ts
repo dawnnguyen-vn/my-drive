fa8b3b0397547d82151403f589b0050d83304a5f
Nguyen Danh <>
2024-06-29 12:38:54 +0700

    validate with zod

--- backend/functions/upload/index.ts
+++ backend/functions/upload/index.ts
@@ -1,7 +1,17 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { APIGatewayProxyEventV2 } from "aws-lambda";
 import { getBoundary, parse } from "parse-multipart-data";
-import { v4 as uuidv4 } from "uuid";
+import { z } from "zod";
+
+class Response {
+  private statusCode: number;
+  private message: string;
+
+  public constructor(statusCode: number, message: string) {
+    this.statusCode = statusCode;
+    this.message = message;
+  }
+}
 
 const client = new S3Client();
 const BUCKET_NAME = process.env.BUCKET_NAME as string;
@@ -10,9 +20,11 @@ const base64ToBuffer = (base64String: string) => {
   return Buffer.from(base64String, "base64");
 };
 
-type Data = {
-  [key: string]: unknown;
-};
+const File = z.object({
+  filename: z.string(),
+  type: z.string(),
+  data: z.instanceof(Buffer),
+});
 
 export const handler = async (event: APIGatewayProxyEventV2) => {
   if (!event.body) return;
@@ -20,16 +32,24 @@ export const handler = async (event: APIGatewayProxyEventV2) => {
     base64ToBuffer(event.body),
     getBoundary(event.headers["content-type"] || "")
   );
-  const data: Data = {};
-  for (let i = 0; i < parts.length; i++) {
-    const part = parts[i];
-    data[part.name as keyof Data] = part.data;
+
+  const fileSchema = File.safeParse(parts[0]);
+  if (fileSchema.error) {
+    return new Response(400, "Cannot parse data");
   }
+  const file = fileSchema.data;
+
   const command = new PutObjectCommand({
     Bucket: BUCKET_NAME,
-    Key: uuidv4(),
-    Body: data.file as Buffer,
+    Key: file.filename + "-" + Date.now(),
+    Body: file.data,
+    ContentType: file.type,
   });
+
   const response = await client.send(command);
-  console.log(response);
+
+  if (response.$metadata.httpStatusCode == 200) {
+    return new Response(201, "Upload success!");
+  }
+  return new Response(400, "Upload fail!");
 };
